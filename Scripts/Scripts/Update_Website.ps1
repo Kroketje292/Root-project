a7ce72e3c245ed842f3c4928ce1aa68508a9741e
Kroketje292 <95290214+Kroketje292@users.noreply.github.com>
2023-09-20 11:46:59 +0200

    Delete Scripts directory

--- Scripts/Scripts/Update_Website.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-# Verander de huidige directory naar de doeldirectory
-Set-Location -Path C:\Website\
-
-# Clone je GitHub-repository naar de doeldirectory
-git clone https://github.com/Kroketje292/uitprobeersel
