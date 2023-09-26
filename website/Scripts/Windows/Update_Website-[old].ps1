a3839dc6e76ef75a2a8066c22419ab5b04fce219
Kroketje292 <95290214+Kroketje292@users.noreply.github.com>
2023-09-26 16:28:40 +0200

    Add files via upload

---
+++ website/Scripts/Windows/Update_Website-[old].ps1
@@ -0,0 +1,6 @@
+﻿
+# Verander de huidige directory naar de doeldirectory
+Set-Location -Path C:\Website\
+
+# Clone je GitHub-repository naar de doeldirectory
+git clone https://github.com/Kroketje292/uitprobeersel
